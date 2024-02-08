a83ff7ab02a876a156ab7e9deb69d45704aa43c4
Viery Nugroho <viery15102002@gmail.com>
2024-02-09 00:21:38 +0700

    set up project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	const queryClient = new QueryClient();
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<ChakraProvider>
+				<Component {...pageProps} />;
+			</ChakraProvider>
+		</QueryClientProvider>
+	);
 }
